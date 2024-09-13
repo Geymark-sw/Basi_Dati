227a5d8a6b4abb6b76c5640d33d5bf57eef096df
Geymark-sw <emmanuelpapio12@gmail.com>
2024-09-13 18:38:22 +0200

    sistemazione Basi Dati

---
+++ Accademia_4.sql
@@ -0,0 +1,32 @@
+create Strutturato as enum(
+
+    'Ricercatore',
+    'Professore Associato',
+    'Professore Ordinario'
+)
+
+create LavoroProgetto as enum(
+
+    'Ricerca e Sviluppo',
+    'Dimostrazione',
+    'Management',
+    'Altro'
+)
+
+create LavoroNonProgettuale as enum(
+
+    'Didattica',
+    'Ricerca',
+    'Missione',
+    'Incontro Dipartimentale',
+    'Icontro Accademico',
+    'Altro'
+)
+
+create CausaAssenza as enum(
+
+    'Chiusura Universitaria',
+    'Maternità',
+    'Malattia'
+)
+
