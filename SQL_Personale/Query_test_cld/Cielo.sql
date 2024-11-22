309f47d92b0b28422ea3357b38f505b28ea888ee
Geymark-sw <emmanuelpapio12@gmail.com>
2024-11-22 18:43:49 +0100

    query 2 cielo

--- SQL_Personale/Query_test_cld/Cielo.sql
+++ SQL_Personale/Query_test_cld/Cielo.sql
@@ -7,3 +7,12 @@ FROM arrpart ap
 JOIN luogoaeroporto la 
 ON ap.arrivo = la.aeroporto
 WHERE la.citta = 'New York'
+
+-- QUERY 2
+/*2. Qual è la durata media di un volo di ogni compagnia
+(restituire nome della compagnia e durata media dei suoi
+voli) [3 punti]*/
+
+SELECT v.comp, AVG(v.durataminuti) 
+FROM volo v
+GROUP BY v.comp
